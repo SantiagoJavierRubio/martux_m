a09ef6839a610d9527d8b9e31320846ba527fb6f
Santiago Javier Rubio <s.rubio91@gmail.com>
2024-06-03 22:39:21 -0300

    feat: add writings

--- app/(pages)/writings/Writing.tsx
+++ app/(pages)/writings/Writing.tsx
@@ -1,56 +1,41 @@
 "use client";
 
+import _ from "lodash";
 import React, { useState } from "react";
-import _ from 'lodash';
 
 type WritingProps = {
   title: string;
-  date: Date;
   text: string;
 };
 
-const LIMIT = 350;
+export default function Writing({ title, text }: WritingProps) {
+  const [isExpanded, setExpanded] = useState<boolean>(false);
 
-export default function Writing({ title, date, text }: WritingProps) {
-  const parsedText = _.unescape(text)
-  const [isExpanded, setExpanded] = useState<boolean>(parsedText.length <= LIMIT);
-
+  console.log(text)
   return (
     <div
-      className={`overflow-y-hidden w-full text-ellipsis p-3 border-2 border-slate-500 rounded-xl`}
+      className={`w-full text-ellipsis p-3 border-2 border-slate-500 rounded-xl`}
     >
-      <h3 className="text-3xl">
-        {title} -{" "}
-        {date.toLocaleDateString("en-US", {
-          month: "2-digit",
-          year: "numeric",
-          day: "2-digit",
-        })}
+      <h3 className="text-3xl capitalize">
+        {title}
       </h3>
-
-      <p className="font-lato max-w-full text-pretty whitespace-pre-wrap">
-        {isExpanded ? parsedText : parsedText.slice(0, LIMIT).trimEnd() + "..."}
-      </p>
-      {parsedText.length > 350 && (
-        <>
-          {isExpanded ? (
-            <button
-              type="button"
-              className="font-semibold text-blue-500 w-full text-right px-3"
-              onClick={() => setExpanded(false)}
-            >
-              Show less
-            </button>
-          ) : (
-            <button
-              type="button"
-              className="font-semibold text-blue-500 w-full text-right px-3"
-              onClick={() => setExpanded(true)}
-            >
-              Read more
-            </button>
-          )}
-        </>
+      <iframe src={`/interviews/${text}`} className={`w-full overflow-clip transition-all ${isExpanded ? 'h-[50svh]':'h-min'}`} onLoad={(e) => console.log(e.target)} />
+      {isExpanded ? (
+        <button
+          type="button"
+          className="font-semibold text-blue-500 w-full text-right px-3"
+          onClick={() => setExpanded(false)}
+        >
+          Show less
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="font-semibold text-blue-500 w-full text-right px-3"
+          onClick={() => setExpanded(true)}
+        >
+          Show more
+        </button>
       )}
     </div>
   );
--- app/(pages)/writings/page.tsx
+++ app/(pages)/writings/page.tsx
@@ -4,6 +4,30 @@ import interviewsCover from "@/public/fotos/Martux 3.jpg";
 import { CoverImageSection } from "../components/CoverImageSection";
 import Writing from "./Writing";
 
+const files = [
+  '-Martusciello-Metaxu.html',
+  'About a silent way 4.html',
+  'About Olindo Fortino.html',
+  'beat-ween zone.html',
+  'Dissectio in Francese.html',
+  'ENG About in a Silent Way 3-1.html',
+  'Essere dei capolavori.html',
+  'Interv auditorium.html',
+  'intervista Blow Up.html',
+  'intervista digicult.html',
+  'Intervista Pereira.html',
+  'Ipercorpo spaesamenti...html',
+  'Le Tre bolle.html',
+  'M-Chion.html',
+  'Martux_m + acustico.html',
+  'Metaxu - Intervista in 80F.html',
+  'metaxu-di massimo Canevacci.html',
+  'NON-DICE.html',
+  'Presentazione Reminescence.html',
+  'Scritto per paolo ruffini 2.html',
+  'testo Reminescence 2.html'
+]
+
 export default async function Music() {
   return (
     <CoverImageSection
@@ -15,49 +39,10 @@ export default async function Music() {
       imgStyles="object-top"
     >
       <div className="min-h-svh flex flex-col gap-6">
-        <Writing title="X-Scape interview" date={new Date()} text={texto} />
+        {files.map(file => 
+          <Writing key={file} title={file.replace('.html', '')} text={file} />
+        )}
       </div>
     </CoverImageSection>
   );
 }
-
-const texto = `Domanda:
-Mi parli di com'è nato questo progetto: com'è nato il concept del live ed in che modo sietegiunti alla produzione del DVD?
-
-M.M.
-Il progetto X-Scape viene da una lunga gestazione, tutto nasce sempre da un desiderio;desiderio di uscire fuori da un linguaggio e noi siamo nel linguaggio e il linguaggio crea dei guasti, è fatto solo di buchi neri, di guasti.Ecco da dove nasce il desiderio X-Scape, lasciare parlare dei significanti, lasciarli andare, esserne parlati, essere finalmente fuori dall’ovvio.“Desiderio” come segreto della seduzione, perchè  qualcosa prima di compiersi, abbia il tempo di mancarvi. Far scaturire l’immagine tale è la potenza, l’urto della copulazione.E da ciò, nasce questo primo mio primo lavoro audiovisuale dal titolo X-Scape, con la preziosissima collaborazione di Mattia Casalegno, che ha arricchito e e sviluppato eccezionalmente questo mio primo progetto.Costruire finalmente un arte che sia irrespirabile, che non sia più consolatoria, che non abbia più la maschera puttanesca della consolazione, che non abbia più il decorativo epoter fare questo, bisogna dimenticare chi si è, perdere ogni identità, lasciare nell’oblio ogni premessa.Emerge allora il rapporto essenziale, la complicità della vista con l’ascolto, a ricercare ciò che è l’oggetto di un oblio della memoria, di un immaginabile nell’immaginazione, di unimpensabile nel pensiero.L’idea successiva a tutto questo è stata quella di produrre un DVD, una una di-scrittura di tutto il lavoro svolto nei live, quindi non più forme, ma rapporti cinematici; non più soggetti, ma individuazioni dinamiche senza soggetto, che costituiscono dei concatenamenti collettivi, un piano della longitudine e della latitudine, che si oppone ad un piano dell’organizzazione, creare degli intermezzi che siano dei focolai di creazione, per essere uno straniero nella propria lingua.
-
-Domanda:
-Quando è cominciato il tuo interesse per la musica concreta?
-
-R:
-E’ iniziato intorno agli anni 90 in un periodo dove le mie esperienze musicali ( rock, jazz ecc.) erano arrivate ad un punto di stasi. Ricordo con quanto ingenuo entusiasmo la scoperta della musica concreta rilanciava la possibilità di continuare in una forma nuova e dinamica un’esperienza con il mondo del suono attraverso una modalità completamente nuova. Il rapporto con la registrazione era stato per me sino a quel momento limitato alla forma documentele di un evento sonoro, da quel momento invece la registrazione e stato quel “principio informe” che poi manipolato, montato, e fissato sul un supporto diventava un “corpo” che si imponeva in un principio compositivo nascosto, trascendente che non è sonoro che non è “udibile” di per se.
-
-Domanda:
-Si potrebbe dire che è nelle performance dal vivo, di fronte ad un pubblico, quando devi sviluppare tutto questo arsenale sonoro che tu usi in modo libero.....Musiche in movimento dove la sperimentazione aleggia, facendo uso di oggetti, motori elettrici e strumenti convenzionali riuscendo ad ottenere una vasta gamma di timbri e intensità strutturata in modo complesso. Risiede qui il tuo interesse?
-
-R:
-Esatto. E’ lo stato di ciò che bisogna chiamare “complicatio”contro la “semplificatio”. I corpi-sonori sono linguaggi perché essenzialmente sono “flessione” la flessione corporea liberatada tutto ciò che ordinariamente la nasconde. Far scaturire l’immagine tale è la potenza, l’urto della copulazione. Linee di articolazione, segmentarietà, strati, territorialità; ma anche linee di fuga, movimenti di deterritorializzazione, velocità comparate, viscosità, contrazioni, spasmi, precipitazione e rottura, azione sui corpi di forze invisibili. Suoni-corpi  sospesi nell’ esitazione che sviluppano tutti i sensi della disgiunzione. Articolazione disgiuntiva , dove i suoni-corpi contengono un linguaggio nascosto, l’argomentazione più astratta.
-
-Domanda:
-Cosa pensi in merito agli incontri di artisti del suono che si incontrano in un certo spazio per discutere esperienze e dare concerti elettroacustici , sound art, musica elettronica e nuova musica? Fino a che punto questi aiutano a fare progetti futuri più concreti?
-
-R:
-Non credo in questo tipo di incontri, questi a mio avviso esistono solo per ragioni di comodità. Tutte queste definizioni costituiscono una formalizzazione, delle parole d’ordine solo per apparati di potere. Il problema non stà nel definire o catalogare quello che si stà facendo, in questo modo si favorisce i gestori dello spettacolo “ i becchini dell’arte”. Bisogna abbandonare ogni forma di concetto solido: elettroacustica, sound art, musica nuova ecc., sono definizioni binarie solide, bisogna proclamare la morte ad ogni forma di dominio che cerca di dare ordine, e autorita dell’uno rispetto all’altro. Bisogna sradicare ogni radice e costruire dei percorsi sessuati a dislocazione di ogni disciplina, di ogni metodo.Domanda:Con quali persone ti senti a tuo agio quando condividi il palcoscenico?Pensi che il coinvolgimento dell'ascoltatore sia fondamentale nei tuoi concerti?
-
-R:
-Dividere il palcoscenico è dividere uno spazio di vita, uno spazio di tempo. E’ uno spazio-tempo dove creare una “articolazione disgiuntiva” , emerge quindi una relazione essenziale, dove esercitare la nostra condotta attiva nei confronti dei riflessi, delle eco e dei doppi, per raccoglierli e per suscitarli, per far sorgere una molteplicità. E’ chiaro che in un concerto, il rapporto con l’ascoltatore costituisce una concatenazione fondametale costituisce una ragione necessaria.
-
-Domanda:
-Potresti, parlarci delle collaborazioni nei tuoi progetti? Che ruolo occupano?
-
-R:
-Quel che conta nei progetti non è nel fatto di lavorare insieme, ma nel lavorare nel mezzo, perchè qualcosa succeda bisogna che ci sia una differenza di potenziale, due livelli, e alloraqualcosa succede. Non si tratta di avere le stesse idee, ma comprendersi senza avere bisogno di spiegarsi. E’ avere un linguaggio comune, o meglio ancora, avere un pre-linguaggio in comune.Un evoluzione a-parallela, produrre qualcosa che non si trova ne nell’uno ne nell’altro, per realizzare una molteplicità. Deterritorializzarsi l’uno nell’altro, reinventare l’alterità, in un rapporto di fascinazione, io ho rubato da loro come spero loro abbiano fatto con me.
-
-Domanda:
-Quale è il futuro delle tue prossime pubblicazioni? Continuerai ad essere fedele ai tuoi concetti elettroacustici e sperimentali?
-
-R: 
-Stiamo chiudendo in questi giorni, un nuovo CD insieme a Danilo Rea, è un lavoro in duo, elettronica e pianoforte è sarà pubblicato da Musica per Roma, E’ un lavoro in cui credo molto, è un incontro fra la melodia e l’elettronica. E’ un lavoro che ci ha richiesto molte energie, ma siamo molto soddisfatti del risultato.
-
-Mi chiedi se rimarro fedele ai miei concetti elettroacustici e sperimentali; al concetto di fedeltà consegue quello di tradimento, e mi viene da dirti che gli empiristi non sono dei teorici, ma degli sperimentatori, non interpretano mai, non hanno principi, ogni sperimentatore deve essere un traditore, il quale non cerca qualcosa da conquistare o un ordine stabilito, ma creare, quindi sparire, diventare sconosciuto, perdere la propria identità, il proprio volto, cercare l’anomalo, il quale si trova sempre alla frontiera, sul margine di un burrone.`;
